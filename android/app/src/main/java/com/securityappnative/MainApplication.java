0f3b5bab384fa4bf57e326b00382bf972c32b9fe
Saim Ahmad <Sahmad5349@conestogac.on.ca>
2020-07-13 19:56:49 -0400

    made changes in panic....mail and sms to multiple people

--- android/app/src/main/java/com/securityappnative/MainApplication.java
+++ android/app/src/main/java/com/securityappnative/MainApplication.java
@@ -1,5 +1,6 @@
 package com.securityappnative;
 
+import com.tkporter.sendsms.SendSMSPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
