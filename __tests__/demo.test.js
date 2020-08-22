f539c81d773d4f01ac96ee1d7b54f91ea07ef42c
May Derbas <mderbas3749@conestogac.on.ca>
2020-08-22 01:48:30 -0400

    delete files

--- __tests__/demo.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Demo from '../components/demo';
-
-import renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-  const tree = renderer.create(<Demo />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
